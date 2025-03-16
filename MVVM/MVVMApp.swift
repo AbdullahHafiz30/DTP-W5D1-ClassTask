664fdecb6a5134cac984d14fa0c8c620db486f2a
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-16 11:26:23 +0300

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Abdullah Hafiz on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
